3b06afc119547b056c0e6ceb0e61bdcee6c74d06
Tucho <tuchobm@gmail.com>
2022-04-24 00:44:34 +0200

    Create colores.sh

---
+++ colores.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Colours
+redColour="\e[0;31m"
+greenColour="\e[0;32m"
+yellowColour="\e[0;33m"
+blueColour="\e[0;34m"
+purpleColour="\e[0;35m"
+turquoiseColour="\e[0;36m"
+grayColour="\e[0;37m"
+
+endColour="\e[m"
